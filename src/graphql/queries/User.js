aeef6af2478d7b5d22b8504b8f7e8f9c83a616f1
Trịnh Duy Hưng <57101685+HUNG-rushb@users.noreply.github.com>
2022-11-13 20:23:00 +0700

    ok

--- src/graphql/queries/User.js
+++ src/graphql/queries/User.js
@@ -1,7 +1,15 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const GET_USERS_ID = gql`
   query GetUsersID {
+    users {
+      id
+    }
+  }
+`;
+
+export const UPDATE_IMAGE = gql`
+  mutation UpdateImage {
     users {
       id
     }
--- src/hooks/uploadImage.js
+++
@@ -1,1 +0,0 @@
-// https://javascript.plainenglish.io/how-to-upload-files-to-aws-s3-in-react-591e533d615e
