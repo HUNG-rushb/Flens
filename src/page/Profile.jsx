b10775680363482030687bc54697609fee32a807
quocthanhh <dangthanh3819@gmail.com>
2022-12-05 19:45:44 +0700

    update profile page

--- src/page/Profile.jsx
+++ src/page/Profile.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import '../components/Proflie/Profile.css';
-// import { Col, Row } from 'react-bootstrap';
+import {Tab, Tabs} from 'react-bootstrap' 
 import { PersonCircle } from 'react-bootstrap-icons';
-import CoverImage from '../assets/profileCoverImage.jpg'
+import CoverImage from '../assets/profileCoverImage.jpg';
+import Activity from '../components/Proflie/ActivityTab.jsx'
+import Portfolio from '../components/Proflie/PortfolioTab.jsx'
+import Bio from '../components/Proflie/BioTab.jsx'
 
 export default function Profile() {
+
   return (
-    <>
-      <img src={CoverImage} alt='' className='coverImage'></img>
+    <div className="profilePage">
+      <div className="overlay"></div>
+      <img src={CoverImage} alt="" className="coverImage"></img>
       <div className="peronalInfor">
-        <div className="profileAvatar">
-          <PersonCircle />
+        <div className="coverImageContent">
+          <PersonCircle size="sm" className="profileAvatar" />
+          <div className="profileName">Nguyen Van A</div>
         </div>
-        <div className="profileName">Nguyen Van A</div>
       </div>
+
       <div className="profile-tabs">
-        <div>Activity</div>
-        <div>PORTFOLIO</div>
-        <div>BIO</div>
+    
+        <Tabs defaultActiveKey='Activity'>
+          <Tab eventKey='Activity' title='Activity'><Activity/></Tab>
+          <Tab eventKey='Portfolio' title='Portfolio'><Portfolio/></Tab>
+          <Tab eventKey='Bio' title='Bio'><Bio/></Tab>
+        </Tabs>
       </div>
-    </>
+    </div>
   );
 }
