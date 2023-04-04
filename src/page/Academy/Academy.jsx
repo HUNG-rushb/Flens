51c77f9d5f102ffc641c1e93c64ddebf9306c922
quocthanhh <dangthanh3819@gmail.com>
2023-04-04 16:12:19 +0700

    mapping function with fake data

--- src/page/Academy/Academy.jsx
+++ src/page/Academy/Academy.jsx
@@ -4,16 +4,68 @@ import InteractiveCourses3 from '../../assets/images/academy/interCourses3.png';
 import InteractiveCourses4 from '../../assets/images/academy/interCourses4.png';
 import VideoCourses1 from '../../assets/images/academy/videoCourses1.png';
 import VideoCourses2 from '../../assets/images/academy/videoCourses2.png';
-import Workshop1 from '../../assets/images/academy/workshop1.png'
-import Workshop2 from '../../assets/images/academy/workshop2.png'
-import Workshop3 from '../../assets/images/academy/workshop3.png'
-
+import Workshop1 from '../../assets/images/academy/workshop1.png';
+import Workshop2 from '../../assets/images/academy/workshop2.png';
+import Workshop3 from '../../assets/images/academy/workshop3.png';
 import Page from '../../components/utils/Page';
 import './Academy.css';
 import React, { Suspense } from 'react';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 
 const Academy = () => {
+  const video_courses_data = [
+    {
+      id: 1,
+      image: VideoCourses1,
+      title: 'Helmut Newton MasterClass',
+    },
+    {
+      id: 2,
+      image: VideoCourses2,
+      title: 'Willy Ronis MasterClass',
+    },
+  ];
+
+  const interactive_courses_data = [
+    {
+      id: 1,
+      image: InteractiveCourses1,
+      title: 'Basics of Photography',
+    },
+    {
+      id: 2,
+      image: InteractiveCourses2,
+      title: 'Shutter Speed',
+    },
+    {
+      id: 3,
+      image: InteractiveCourses3,
+      title: 'Basic composition',
+    },
+    {
+      id: 4,
+      image: InteractiveCourses4,
+      title: 'Light',
+    },
+  ];
+
+  const workshop_data = [
+    {
+      id: 1,
+      image: Workshop1,
+      title: 'Mosaic Art Workshop',
+    },
+    {
+      id: 2,
+      image: Workshop2,
+      title: 'Art Macrame Workshop',
+    },
+    {
+      id: 3,
+      image: Workshop3,
+      title: 'Meow pottery Workshop',
+    },
+  ];
   return (
     <Page title={'Flens-Academy'}>
       <Suspense fallback={null}>
@@ -36,17 +88,25 @@ const Academy = () => {
               </div>
 
               <div className="video-courses-body">
-                <div>
-                  <img src={VideoCourses1} alt="videoCourses" width={500}></img>
-                  <span>Helmut Newton MasterClass</span>
-                </div>
-                <div>
-                  <img src={VideoCourses2} alt="videoCourses" width={500}></img>
-                  <span>Willy Ronis MasterClass</span>
-                </div>
-                <div className="next-button">
-                  <ArrowRightCircle size={30} />
-                </div>
+                {video_courses_data.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <img
+                        src={item.image}
+                        alt="videoCourses"
+                        width={500}
+                      ></img>
+                      <span>{item.title}</span>
+                    </div>
+                  );
+                })}
+                {video_courses_data.length > 1 ? (
+                  <div className="next-button">
+                    <ArrowRightCircle size={30} />
+                  </div>
+                ) : (
+                  <></>
+                )}
               </div>
             </div>
 
@@ -56,41 +116,25 @@ const Academy = () => {
                 <span>Show all</span>
               </div>
               <div className="interactive-body-part">
-                <div>
-                  <img
-                    src={InteractiveCourses1}
-                    alt="interactiveCourses1"
-                    height={350}
-                  ></img>
-                  <span>Basics of Photography</span>
-                </div>
-                <div>
-                  <img
-                    src={InteractiveCourses2}
-                    alt="interactiveCourses2"
-                    height={350}
-                  ></img>
-                  <span>Shutter Speed</span>
-                </div>
-                <div>
-                  <img
-                    src={InteractiveCourses3}
-                    alt="interactiveCourses3"
-                    height={350}
-                  ></img>
-                  <span>Basic composition</span>
-                </div>
-                <div>
-                  <img
-                    src={InteractiveCourses4}
-                    alt="interactiveCourses4"
-                    height={350}
-                  ></img>
-                  <span>Light</span>
-                </div>
-                <div className="next-button">
-                  <ArrowRightCircle size={30} />
-                </div>
+                {interactive_courses_data.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <img
+                        src={item.image}
+                        alt="interactiveCourses4"
+                        height={350}
+                      ></img>
+                      <span>{item.title}</span>
+                    </div>
+                  );
+                })}
+                {interactive_courses_data.length > 3 ? (
+                  <div className="next-button">
+                    <ArrowRightCircle size={30} />
+                  </div>
+                ) : (
+                  <></>
+                )}
               </div>
             </div>
 
@@ -100,23 +144,22 @@ const Academy = () => {
                 <span>Show all</span>
               </div>
               <div className="workshop-body-part">
-                <div>
-                  <img src={Workshop1} alt="workshop" width={300}></img>
-                  <span>Mosaic Art Workshop</span>
-                </div>
-                <div>
-                  <img src={Workshop2} alt="workshop" width={400}></img>
-                  <span>Art Macrame Workshop</span>
-                </div>
-                <div>
-                  <img src={Workshop3} alt="workshop" width={300}></img>
-                  <span>Meow pottery Workshop</span>
-                </div>
-                <div className="next-button">
-                  <ArrowRightCircle size={30} />
-                </div>
+                {workshop_data.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <img src={item.image} alt="workshop" height={300}></img>
+                      <span>{item.title}</span>
+                    </div>
+                  );
+                })}
+                {workshop_data.length > 2 ? (
+                  <div className="next-button">
+                    <ArrowRightCircle size={30} />
+                  </div>
+                ) : (
+                  <></>
+                )}
               </div>
-              
             </div>
           </div>
         </div>
