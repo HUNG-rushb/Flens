5fbd3bc00f712669767e7b404257a8deee3f3d76
quocthanhh <dangthanh3819@gmail.com>
2023-06-26 00:16:24 +0700

    add select category

--- src/page/UploadImage/UploadImage.jsx
+++ src/page/UploadImage/UploadImage.jsx
@@ -34,6 +34,9 @@ const UploadImage = () => {
     id: 0,
     value: '',
   });
+  const [selectedCatagory, setSelectedCategory] = useState('');
+
+  const options = ['Family', 'Fashion', 'Food'];
   const [copyright, setCopyright] = useState('');
 
   const { createPost, isFetching, fetchedData, fetchError } =
@@ -55,8 +58,8 @@ const UploadImage = () => {
   };
 
   const removeTag = (id) => {
-    const removeTag = tags.filter((item) => item.id !==id);
-    setTags(removeTag)
+    const removeTag = tags.filter((item) => item.id !== id);
+    setTags(removeTag);
   };
 
   const handleFileSelect = () => {
@@ -106,46 +109,42 @@ const UploadImage = () => {
   };
 
   // Create Post
-  // const handleConfirmUpload = async (event) => {
-  //   event.preventDefault();
+  const handleConfirmUpload = async (event) => {
+    event.preventDefault();
 
-  //   const result = await uploadImageToAWS({ selectedFile });
-  //   console.log({ result });
+    const result = await uploadImageToAWS({ selectedFile });
+    console.log({ result });
 
-  //   try {
-  //     await createPost({
-  //       variables: {
-  //         createPostData: {
-  //           categoryId: '6496c183518d8caaf82fcaca',
-  //           userId,
-  //           title,
-  //           aperture,
-  //           lens,
-  //           takenWhen,
-  //           camera,
-  //           focalLength,
-  //           shutterSpeed,
-  //           ISO: iso,
-  //           copyRight: copyright,
-  //           imageHash: '',
-  //           imageURL: result.Location,
-  //         },
-  //       },
-  //     });
+    try {
+      await createPost({
+        variables: {
+          createPostData: {
+            categoryId: '6496c183518d8caaf82fcaca',
+            userId,
+            title,
+            aperture,
+            lens,
+            takenWhen,
+            camera,
+            focalLength,
+            shutterSpeed,
+            ISO: iso,
+            copyRight: copyright,
+            imageHash: '',
+            imageURL: result.Location,
+          },
+        },
+      });
 
-  //     // navigate('/');
-  //   } catch (e) {
-  //     throw e;
-  //   }
+      // navigate('/');
+    } catch (e) {
+      throw e;
+    }
 
-  //   // console.log(fetchError);
-  //   if (!fetchError) {
-  //     navigate('/');
-  //   }
-  // };
-
-  const handleConfirmUpload = () => {
-    console.log('upload');
+    // console.log(fetchError);
+    if (!fetchError) {
+      navigate('/');
+    }
   };
 
   const handleCancelUpload = (event) => {
@@ -190,49 +189,49 @@ const UploadImage = () => {
                   <div className="modal-upload-right">
                     <div className="modal-upload-details">
                       <div>
-                        <label htmlFor="">Title</label>
+                        <label>Title</label>
                         <input
                           type="text"
                           value={title}
-                          placeholder="input tittle for image"
+                          placeholder="input tittle for this image"
                           onChange={(event) => setTitle(event.target.value)}
                         />
                       </div>
                       <div>
-                        <label htmlFor="">Camera</label>
+                        <label>Camera</label>
                         <input
                           type="text"
-                          placeholder="Input your Camera"
+                          placeholder="Input Camera"
                           value={camera}
                           onChange={(event) => setCamera(event.target.value)}
                         />
                       </div>
 
                       <div>
-                        <label htmlFor="">Lens</label>
+                        <label>Lens</label>
                         <input
                           type="text"
-                          placeholder="Input your Lens"
+                          placeholder="Input Lens"
                           value={lens}
                           onChange={(event) => setLens(event.target.value)}
                         />
                       </div>
 
                       <div>
-                        <label htmlFor="">Aperture</label>
+                        <label>Aperture</label>
                         <input
                           type="text"
-                          placeholder="Input your Aperture"
+                          placeholder="Input Aperture"
                           value={aperture !== '' ? 'f/' + aperture : ''}
                           onChange={(event) => setAperture(event.target.value)}
                         />
                       </div>
 
                       <div>
-                        <label htmlFor="">Shutter speed</label>
+                        <label>Shutter speed</label>
                         <input
                           type="text"
-                          placeholder="Input your Shutter speed"
+                          placeholder="Input Shutter speed"
                           value={shutterSpeed}
                           onChange={(event) =>
                             setShutterSpeed(event.target.value)
@@ -241,7 +240,7 @@ const UploadImage = () => {
                       </div>
 
                       <div>
-                        <label htmlFor="">Taken</label>
+                        <label>Taken</label>
                         <input
                           type="text"
                           placeholder="Taken when"
@@ -251,10 +250,10 @@ const UploadImage = () => {
                       </div>
 
                       <div>
-                        <label htmlFor="">Focal length</label>
+                        <label>Focal length</label>
                         <input
                           type="text"
-                          placeholder="Input your Focal length"
+                          placeholder="Input Focal length"
                           value={focalLength !== '' ? focalLength + 'mm' : ''}
                           onChange={(event) =>
                             setFocalLength(event.target.value)
@@ -263,10 +262,10 @@ const UploadImage = () => {
                       </div>
 
                       <div>
-                        <label htmlFor="">ISO</label>
+                        <label>ISO</label>
                         <input
                           type="text"
-                          placeholder="Input your ISO"
+                          placeholder="Input ISO"
                           value={iso}
                           onChange={(event) => setIso(event.target.value)}
                         />
@@ -274,25 +273,30 @@ const UploadImage = () => {
 
                       <div>
                         <label>Tags</label>
-                        {tags.length > 0 && <div className="all-tags">
-                          {tags.map((item) => {
-                            return (
-                              <div
-                                key={item.id}
-                                onClick={() => removeTag(item.id)}
-                              >
-                                <span id="remove-tag">X</span>
-                                {item.value}
-                              </div>
-                            );
-                          })}
-                        </div>}
+                        {tags.length > 0 && (
+                          <div className="all-tags">
+                            {tags.map((item) => {
+                              return (
+                                <div
+                                  key={item.id}
+                                  onClick={() => removeTag(item.id)}
+                                >
+                                  <span id="remove-tag">X</span>
+                                  {item.value}
+                                </div>
+                              );
+                            })}
+                          </div>
+                        )}
                         <input
                           type="text"
                           placeholder="Input a tag and press enter"
                           onChange={(e) =>
                             setTag({
-                              id: tags.length===0? 1 : tags[tags.length -1].id +1,
+                              id:
+                                tags.length === 0
+                                  ? 1
+                                  : tags[tags.length - 1].id + 1,
                               value: e.target.value,
                             })
                           }
@@ -300,6 +304,23 @@ const UploadImage = () => {
                           value={tag.value}
                         />
                       </div>
+
+                      <div className="image-categories">
+                        <label>Category</label>
+                        <select
+                          onChange={(e) => setSelectedCategory(e.target.value)}
+                          id="select-image-category"
+                        >
+                          {options.map((item) => {
+                            return (
+                              <option key={options.indexOf(item)}>
+                                {item}
+                              </option>
+                            );
+                          })}
+                        </select>
+                      </div>
+
                       <div>
                         <label htmlFor="">CopyRight</label>
                         <input
