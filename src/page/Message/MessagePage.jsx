5375e0093ba644e4b37f2c11e1ae538127cfee5b
Trịnh Duy Hưng <57101685+HUNG-rushb@users.noreply.github.com>
2023-11-10 15:22:47 +0700

    webpack

--- src/page/Message/MessagePage.jsx
+++ src/page/Message/MessagePage.jsx
@@ -1,16 +1,15 @@
 import { useAuthState } from '../../context/AuthContext';
 import './Message.css';
-// import { ChatApp } from 'chatApp/ChatApp';
+// import { ChatApp } from 'chat/ChatApp';
 import React, { useState, useEffect, useRef } from 'react';
 
 // https://dev.to/devsmitra/the-complete-guide-to-micro-frontend-with-reactjs-for-2022-36b2
-
-// import chatApp
 
 const MessageChatApp = () => {
   const { id: userId } = useAuthState();
 
   // return <ChatApp userId={userId} />;
+  return <p>chat</p>;
 };
 
 export default MessageChatApp;
