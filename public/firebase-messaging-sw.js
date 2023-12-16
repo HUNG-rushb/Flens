a7d5ec13bd6baead9ca29929856b8c729a94e5d4
Trịnh Duy Hưng <57101685+HUNG-rushb@users.noreply.github.com>
2023-12-17 01:16:24 +0700

    test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,48 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
-
-// self.addEventListener('fetch', () => {
-//   const urlParams = new URLSearchParams(location.search);
-//   self.firebaseConfig = Object.fromEntries(urlParams);
-// });
-
-// const defaultConfig = {
-//   apiKey: true,
-//   projectId: true,
-//   messagingSenderId: true,
-//   appId: true,
+// importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+// // self.addEventListener('fetch', () => {
+// //   const urlParams = new URLSearchParams(location.search);
+// //   self.firebaseConfig = Object.fromEntries(urlParams);
+// // });
+// // const defaultConfig = {
+// //   apiKey: true,
+// //   projectId: true,
+// //   messagingSenderId: true,
+// //   appId: true,
+// // };
+// // firebase.initializeApp(self.firebaseConfig || defaultConfig);
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDFNig0B0NfjfafWksbULgXcGvOhagUBBo',
+//   authDomain: 'noti-flens.firebaseapp.com',
+//   databaseURL:
+//     'https://noti-flens-default-rtdb.asia-southeast1.firebasedatabase.app',
+//   projectId: 'noti-flens',
+//   storageBucket: 'noti-flens.appspot.com',
+//   messagingSenderId: '1050901670890',
+//   appId: '1:1050901670890:web:5c4bf14e709bf6bce3e093',
+//   measurementId: 'G-FZ37LB7Q55',
 // };
-// firebase.initializeApp(self.firebaseConfig || defaultConfig);
+// firebase.initializeApp(firebaseConfig);
+// if (firebase.messaging.isSupported()) {
+//   const messaging = firebase.messaging();
+//   const channel = new BroadcastChannel('notifications');
+//   messaging.onBackgroundMessage((payload) => {
+//     channel.postMessage(payload);
+//   });
+// }
+// https://viblo.asia/p/reactjs-push-notification-su-dung-firebase-cloud-messaging-yZjJYE9XJOE
+import firebase from 'firebase/app/dist/index.esm.js';
+import 'firebase/messaging/dist/index.esm.js';
 
-const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+// Scripts for firebase and firebase messaging
+// importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+
+const vapidKey =
+  'BDdytKBVZZB-irKRkBlQOnOyoLcfgB6bNKGUmnaISHB-4gS4IiNL_i-1QtNikVUr5uphGWFC0PjP7Hp6rklBHcE';
+
+export const firebaseConfig = {
   apiKey: 'AIzaSyDFNig0B0NfjfafWksbULgXcGvOhagUBBo',
   authDomain: 'noti-flens.firebaseapp.com',
   databaseURL:
@@ -26,13 +54,41 @@ const firebaseConfig = {
   measurementId: 'G-FZ37LB7Q55',
 };
 
-firebase.initializeApp(firebaseConfig);
-
-if (firebase.messaging.isSupported()) {
-  const messaging = firebase.messaging();
-  const channel = new BroadcastChannel('notifications');
-
-  messaging.onBackgroundMessage((payload) => {
-    channel.postMessage(payload);
-  });
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
 }
+
+let messaging;
+
+if (typeof window !== 'undefined') {
+  if (firebase.messaging.isSupported()) {
+    messaging = firebase.messaging();
+  }
+}
+
+export const getMessagingToken = async () => {
+  let currentToken = '';
+
+  if (!messaging) return;
+
+  try {
+    currentToken = await messaging.getToken({
+      vapidKey: vapidKey,
+    });
+
+    console.log('FCM registration token', currentToken);
+  } catch (error) {
+    console.log('An error occurred while retrieving token. ', error);
+  }
+
+  return currentToken;
+};
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    messaging.onMessage((payload) => {
+      resolve(payload);
+    });
+  });
