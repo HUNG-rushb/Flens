429f31509fa032cf12b81ce8fa6ac5fadf83c967
Trịnh Duy Hưng <57101685+HUNG-rushb@users.noreply.github.com>
2023-12-17 00:18:39 +0700

    ok

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,10 +1,17 @@
 import { firebaseConfig } from './firebase';
 
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/<v9+>/firebase-app-compat.js'
+// );
+// importScripts(
+//   'https://www.gstatic.com/firebasejs/<v9+>/firebase-messaging-compat.js'
+// );
+
 if ('serviceWorker' in navigator) {
   const firebaseConfigParams = new URLSearchParams(firebaseConfig).toString();
   navigator.serviceWorker
-    // .register(`../firebase-messaging-sw.js?${firebaseConfigParams}`)
-    .register(`../public/firebase-messaging-sw.js?${firebaseConfigParams}`)
+    .register(`../firebase-messaging-sw.js?${firebaseConfigParams}`)
+    // .register(`../public/firebase-messaging-sw.js?${firebaseConfigParams}`)
     .then(function (registration) {
       console.log('Registration successful, scope is:', registration.scope);
     })
